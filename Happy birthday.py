ba81d05187e8ac0abd4b6a5fe0b25acb52314d35
vikaswilson1980-creator <vikaswilson1980@gmail.com>
2025-09-24 19:45:46 +0530

    Add files via upload

---
+++ Happy birthday.py
@@ -0,0 +1,14 @@
+name=input("enter your name:")
+print("\n Happy birthday",name,)
+date="When does your birthday come"
+print(date)
+a=16 
+print(a)
+b=17 
+print(b)
+c=18 
+print(c)
+d=19 
+print(d)
+e=15 
+print(e)
